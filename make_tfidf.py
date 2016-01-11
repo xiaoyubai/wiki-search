2a35a08c2595a025dd14dc9ff6fda1b767f477fe
xiaoyubai <hawkingbai@gmail.com>
2016-01-11 14:13:44 -0800

    Update make_tfidf.py

--- make_tfidf.py
+++ make_tfidf.py
@@ -40,26 +40,10 @@ class TfToken(object):
     def create_link(self, aws_link):
         self.link = 's3n://%s:%s@%s' % (self.access_key, self.secret_access_key, aws_link)
 
-    def _tokenizing(self, text):
-        regex = re.compile('<.+?>|[^a-zA-Z]')
-        clean_txt = regex.sub(' ', text)
-        tokens = clean_txt.split()
-        lowercased = [t.lower() for t in tokens]
-
-        no_punctuation = []
-        for word in lowercased:
-            punct_removed = ''.join([letter for letter in word if not letter in PUNCTUATION])
-            no_punctuation.append(punct_removed)
-        no_stopwords = [w for w in no_punctuation if not w in STOPWORDS]
-
-        STEMMER = PorterStemmer()
-        stemmed = [STEMMER.stem(w) for w in no_stopwords]
-        return [w for w in stemmed if w]
-
     def _create_rdd(self):
         wiki_rdd = self.sc.textFile(self.link)
         wiki_no_redirect_rdd = wiki_rdd.filter(lambda line: '#REDIRECT' not in line)
-        self.token_rdd = wiki_no_redirect_rdd.map(self._tokenizing)
+        self.token_rdd = wiki_no_redirect_rdd.map(tokenizing)
 
     def tfidf(self):
         self._create_rdd()
@@ -68,3 +52,19 @@ class TfToken(object):
         idf = IDF(minDocFreq=2).fit(tf)
         tfidf = idf.transform(tf)
         return tfidf
+        
+def tokenizing(text):
+    regex = re.compile('<.+?>|[^a-zA-Z]')
+    clean_txt = regex.sub(' ', text)
+    tokens = clean_txt.split()
+    lowercased = [t.lower() for t in tokens]
+
+    no_punctuation = []
+    for word in lowercased:
+        punct_removed = ''.join([letter for letter in word if not letter in PUNCTUATION])
+        no_punctuation.append(punct_removed)
+    no_stopwords = [w for w in no_punctuation if not w in STOPWORDS]
+
+    STEMMER = PorterStemmer()
+    stemmed = [STEMMER.stem(w) for w in no_stopwords]
+    return [w for w in stemmed if w]
