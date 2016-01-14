a9c3421518b8697bcdb4143f524527ea2bbfa99b
cruntar <hawkingbai@gmail.com>
2016-01-14 00:02:52 -0800

    test

--- combined.py
+++ combined.py
@@ -278,7 +278,7 @@ if __name__ == '__main__':
     category = "statistics math math"
 
     sc = ps.SparkContext()
-    aws_link = "jyt109/wiki_articles"
+    aws_link = "wikisample10/sample2"
 
     filename="../keypair.json"
     with open(filename) as f:
